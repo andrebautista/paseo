03cac8b1e72327e9ad391af6224a94845119b18b
Andre <andrebautista2513@gmail.com>
2013-07-11 15:52:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaseoWebsite::Application.config.secret_key_base = '7565c55e2e50ae679fe7c030da097a60ff12915a253c25644b5d6d1ecb105e5834ff66eb2339dc949cba4f0a55e124661767a84beaf80bd6a065d0cd0bb44b18'

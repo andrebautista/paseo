b6c129f2b62f117c0d7e24a661f406b182a608bb
Andre <andrebautista2513@gmail.com>
2013-08-14 14:35:25 -0700

    changed press slightly and added a lighter gray background color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def full_title(page_title)
     base_title = "Paseo"
     if page_title.empty?
-      base_title
+      base_title + " Caribbean"
     else
       "#{base_title} - #{page_title}"
     end
